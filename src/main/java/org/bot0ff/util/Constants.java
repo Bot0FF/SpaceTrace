0fe49a122c1960965912891cf7df2519eff9cfd4
Bot0FF <92510704+Bot0FF@users.noreply.github.com>
2023-10-14 08:40:23 +0300

    add move user

--- src/main/java/org/bot0ff/util/Constants.java
+++ src/main/java/org/bot0ff/util/Constants.java
@@ -3,6 +3,6 @@ package org.bot0ff.util;
 public class Constants {
     public static int MIN_POS_X = 1;
     public static int MAX_POS_X = 10;
-    public static int MIN_POS_Y = 0;
+    public static int MIN_POS_Y = 1;
     public static int MAX_POS_Y = 10;
 }
